134829b55153d2c5db6c7b1cc819ebbc34b86bf2
Jeffrey Wardian <krinsdeath@hotmail.com>
2012-07-02 11:56:47 -0700

    Fixed imports.

--- src/main/java/net/krinsoft/privileges/listeners/PlayerListener.java
+++ src/main/java/net/krinsoft/privileges/listeners/PlayerListener.java
@@ -7,7 +7,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 /**
  *
